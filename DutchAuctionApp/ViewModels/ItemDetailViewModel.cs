20933ee8a42e2bc906f0fd456272099888fe285d
Ron Burch <r@stoate.ch>
2018-05-20 15:48:10 +0200

    initial project

---
+++ DutchAuctionApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DutchAuctionApp.Models;
+
+namespace DutchAuctionApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
